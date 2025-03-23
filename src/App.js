a08167e7957d26f5395adf9c5906cb54dd1f978d
Gurjeet Singh <hit19ece.gurjeetsingh@gmail.com>
2025-03-23 15:21:27 +0530

    solar system

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import CarouselComponent from './component/CarouselComponent';
 import Calculator from './component/Calculator';
+import Solar from './component/Solar';
 function App() {
   return (
     <>
       <CarouselComponent/>
       <Calculator/>
+      <Solar/>
 
       </>
       
---
+++ src/component/Solar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Solar.css'
+const Solar = () => {
+  return (
+    <div className="solar-system">
+      <div className="sun"></div>
+      <div className="planet mercury"></div>
+      <div className="planet venus"></div>
+      <div className="planet earth"></div>
+      <div className="planet mars"></div>
+      <div className="planet jupiter"></div>
+      <div className="planet saturn"></div>
+      <div className="planet uranus"></div>
+      <div className="planet neptune"></div>
+    </div>
+  )
+}
+
+export default Solar
